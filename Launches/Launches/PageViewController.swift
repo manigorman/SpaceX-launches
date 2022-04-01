cf03c2676fd72b5b57bc450248a3f0b9492032bd
manigorman <immanakov@gmail.com>
2022-04-01 18:58:58 +0300

    Created PageVC with pageController

---
+++ Launches/Launches/PageViewController.swift
@@ -0,0 +1,152 @@
+//
+//  ViewController.swift
+//  Launches
+//
+//  Created by Igor Manakov on 30.03.2022.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    // MARK: - Properties
+    
+    private var pages = [UIViewController]()
+    
+    private let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.currentPageIndicatorTintColor = .black
+        pageControl.pageIndicatorTintColor = .systemGray2
+        
+        return pageControl
+    }()
+    
+    private let initialPage = 0
+    
+    // MARK: - Init
+    
+    init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+        setDelegate()
+    }
+    
+    // MARK: - Methods
+    
+    func setupViews() {
+        
+        view.addSubview(pageControl)
+        pageControl.addTarget(self, action: #selector(pageControlTapped(_:)), for: .valueChanged)
+        
+        // create an array of viewController
+        let page1 = ViewController1()
+        let page2 = ViewController2()
+        let page3 = ViewController3()
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = initialPage
+        
+        setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
+    }
+    
+    private func setDelegate() {
+        
+        dataSource = self
+        delegate = self
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            pageControl.widthAnchor.constraint(equalTo: view.widthAnchor),
+            pageControl.heightAnchor.constraint(equalToConstant: 20),
+            view.bottomAnchor.constraint(equalToSystemSpacingBelow: pageControl.bottomAnchor, multiplier: 1),
+        ])
+    }
+    
+    // MARK: - Selectors
+    
+    @objc private func pageControlTapped(_ sender: UIPageControl) {
+        setViewControllers([pages[sender.currentPage]], direction: .forward, animated: true, completion: nil)
+    }
+}
+
+// MARK: - DataSources
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        
+        if currentIndex == 0 {
+            return pages.last               // wrap to last
+        } else {
+            return pages[currentIndex - 1]  // go previous
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+
+        if currentIndex < pages.count - 1 {
+            return pages[currentIndex + 1]  // go next
+        } else {
+            return pages.first              // wrap to first
+        }
+    }
+}
+
+// MARK: - Delegates
+
+extension PageViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        guard let viewControllers = pageViewController.viewControllers else { return }
+        guard let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
+        
+        pageControl.currentPage = currentIndex
+    }
+}
+
+// MARK: - ViewControllers
+
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class ViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
